036a194e50cd3181bc70444c872c375937b4cf2b
Valianti4 <109241884+Valianti4@users.noreply.github.com>
2022-09-24 02:24:44 +0400

    Решил задачу 09

---
+++ Practice02/Example09/Example09.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая выводит случайное число из отрезка [10, 99]
+и показывает наибольшую цифру числа.
+78 -> 8
+12 -> 2
+85 -> 8
+*/
+
+
+Console.WriteLine("Чтобы узнать наибольшую цифру числа из диапазона [10, 99], нажмите клавишу Enter. ");
+Console.ReadLine();
+int num = new Random().Next(10, 100);
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+
+if (firstDigit == secondDigit) Console.WriteLine("Цифры равны. ");
+else if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {num} равна {firstDigit}. ");
+else Console.WriteLine($"Наибольшая цифра числа {num} равна {secondDigit}. ");
---
+++ Practice02/Example09_var2/Example09_var2.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая выводит случайное число из отрезка [10, 99]
+и показывает наибольшую цифру числа.
+78 -> 8
+12 -> 2
+85 -> 8
+Решение с помощью тернарного оператора.
+*/
+
+
+Console.WriteLine("Чтобы узнать наибольшую цифру числа из диапазона [10, 99], нажмите клавишу Enter. ");
+Console.ReadLine();
+int num = new Random().Next(10, 100);
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+string res = firstDigit == secondDigit ? "цифры равны"
+: firstDigit > secondDigit ? firstDigit.ToString() : secondDigit.ToString();
+Console.WriteLine($"Наибольшая цифра числа {num} -> {res}. ");
